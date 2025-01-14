91e9d2f0176e73bdbdbe58a0e8027b307ece39be
dimanoga <dima.yanvarev@gmail.com>
2025-01-14 23:58:59 +0600

    some fixes

--- app/routers/releases_router.py
+++ app/routers/releases_router.py
@@ -13,7 +13,7 @@ from sql_app.database import get_database
 from sql_app.models.user import RolesEnum
 from sql_app.platforms_service import get_platform
 from sql_app.releases_service import get_all_releases, update_release, get_release, get_all_release_types
-from schemas import ReleaseStageCreate, User, ReleaseStageOut, ReleaseTypeOut
+from schemas import ReleaseStageCreate, User, ReleaseStageOut, ReleaseTypeOut, ReleaseStageOutWithFeature
 from auth import get_current_user
 
 router = APIRouter(prefix="/releases", tags=["releases"])
@@ -23,99 +23,101 @@ db_session = Annotated[Session, Depends(get_database)]
 
 @router.post("/", response_model=ReleaseStageOut)
 def create_release(stage: ReleaseStageCreate,
-                   current_user: get_current_user,
-                   db: db_session,
-                   ):
-    if current_user.role not in [RolesEnum.ADMIN, RolesEnum.RELEASE_MANAGER]:
-        raise HTTPException(status_code=403, detail="Not enough permissions")
-    if get_release(name=stage.name, db=db):
-        raise HTTPException(status_code=400, detail="Release stage with this name already exists")
-    if not get_channel(channel_id=stage.channel_id, db=db):
-        raise HTTPException(status_code=404, detail="Channel not found")
-    if not get_platform(platform_id=stage.platform_id, db=db):
-        raise HTTPException(status_code=404, detail="Platform not found")
-    release = releases_service.create_release(stage=stage, db=db)
-    return release
+				   current_user: get_current_user,
+				   db: db_session,
+				   ):
+	if current_user.role not in [RolesEnum.ADMIN, RolesEnum.RELEASE_MANAGER]:
+		raise HTTPException(status_code=403, detail="Not enough permissions")
+	if get_release(name=stage.name, db=db):
+		raise HTTPException(status_code=400, detail="Release stage with this name already exists")
+	if not get_channel(channel_id=stage.channel_id, db=db):
+		raise HTTPException(status_code=404, detail="Channel not found")
+	if not get_platform(platform_id=stage.platform_id, db=db):
+		raise HTTPException(status_code=404, detail="Platform not found")
+	release = releases_service.create_release(stage=stage, db=db)
+	return release
 
 
 @router.get("/all", response_model=list[ReleaseStageOut])
 def get_all_releases(db: db_session):
-    releases = releases_service.get_all_releases(db=db)
-    return releases
+	releases = releases_service.get_all_releases(db=db)
+	return releases
 
 
 @router.get('/{release_id}', status_code=200)
 def get_release(release_id: int, db: db_session):
-    release = releases_service.get_release_with_features(release_id=release_id, db=db)
-    if not release:
-        raise HTTPException(status_code=404, detail="Release stage not found")
-    return release
+	release = releases_service.get_release_with_features(release_id=release_id, db=db)
+	if not release:
+		raise HTTPException(status_code=404, detail="Release not found")
+	response = ReleaseStageOutWithFeature.from_orm(release.Release)
+	response.features = release.features
+	return response
 
 
 @router.delete("/{release_id}", status_code=204)
 def delete_release(release_id: int,
-                   current_user: get_current_user,
-                   db: db_session):
-    if current_user.role not in [RolesEnum.ADMIN, RolesEnum.RELEASE_MANAGER]:
-        raise HTTPException(status_code=403, detail="Not enough permissions")
-    release = releases_service.get_release(release_id=release_id, db=db)
-    if not release:
-        raise HTTPException(status_code=404, detail="Release stage not found")
-    return releases_service.delete_release(stage_id=release_id, db=db)
+				   current_user: get_current_user,
+				   db: db_session):
+	if current_user.role not in [RolesEnum.ADMIN, RolesEnum.RELEASE_MANAGER]:
+		raise HTTPException(status_code=403, detail="Not enough permissions")
+	release = releases_service.get_release(release_id=release_id, db=db)
+	if not release:
+		raise HTTPException(status_code=404, detail="Release stage not found")
+	return releases_service.delete_release(stage_id=release_id, db=db)
 
 
 @router.get("/report", response_class=FileResponse)
 def generate_report(db: db_session):
-    stages = get_all_releases(db=db)
-    doc = Document()
-    doc.add_heading("Release Stages Report", level=1)
-    for stage in stages:
-        doc.add_heading(stage.name, level=2)
-        doc.add_paragraph(f"Description: {stage.description}")
-        doc.add_paragraph(f"Start Date: {stage.start_date}")
-        doc.add_paragraph(f"End Date: {stage.end_date}")
-        doc.add_paragraph(f"Status: {stage.status.value}")
-    file_path = "release_report.docx"
-    doc.save(file_path)
-    return FileResponse(file_path,
-                        media_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document',
-                        filename=file_path)
+	stages = get_all_releases(db=db)
+	doc = Document()
+	doc.add_heading("Release Stages Report", level=1)
+	for stage in stages:
+		doc.add_heading(stage.name, level=2)
+		doc.add_paragraph(f"Description: {stage.description}")
+		doc.add_paragraph(f"Start Date: {stage.start_date}")
+		doc.add_paragraph(f"End Date: {stage.end_date}")
+		doc.add_paragraph(f"Status: {stage.status.value}")
+	file_path = "release_report.docx"
+	doc.save(file_path)
+	return FileResponse(file_path,
+						media_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+						filename=file_path)
 
 
 @router.put("/{stage_id}")
 def update_release(stage_id: int,
-                   name: str | None,
-                   description: str | None,
-                   start_date: str | None,
-                   end_date: str | None,
-                   current_user: get_current_user,
-                   db: db_session
-                   ):
-    if current_user.role not in [RolesEnum.ADMIN, RolesEnum.RELEASE_MANAGER]:
-        raise HTTPException(status_code=403, detail="Not enough permissions")
-    return update_release(stage_id=stage_id,
-                          name=name,
-                          description=description,
-                          start_date=start_date,
-                          end_date=end_date,
-                          db=db)
+				   name: str | None,
+				   description: str | None,
+				   start_date: str | None,
+				   end_date: str | None,
+				   current_user: get_current_user,
+				   db: db_session
+				   ):
+	if current_user.role not in [RolesEnum.ADMIN, RolesEnum.RELEASE_MANAGER]:
+		raise HTTPException(status_code=403, detail="Not enough permissions")
+	return update_release(stage_id=stage_id,
+						  name=name,
+						  description=description,
+						  start_date=start_date,
+						  end_date=end_date,
+						  db=db)
 
 
-@router.get('/types', response_model=list[ReleaseTypeOut], status_code=200)
+@router.get('/types/', response_model=list[ReleaseTypeOut], status_code=200)
 def get_release_types(db: db_session):
-    return get_all_release_types(db=db)
+	return get_all_release_types(db=db)
 
 
-@router.post("/types", response_model=ReleaseTypeOut, status_code=201)
+@router.post("/types/", response_model=ReleaseTypeOut, status_code=201)
 def create_release_type(name: str,
-                        platform_id: int,
-                        channel_id: int,
-                        current_user: get_current_user,
-                        db: db_session):
-    if current_user.role not in [RolesEnum.ADMIN, RolesEnum.RELEASE_MANAGER]:
-        raise HTTPException(status_code=403, detail="Not enough permissions")
-    if not get_platform(platform_id=platform_id, db=db):
-        raise HTTPException(status_code=404, detail="Platform not found")
-    if not get_channel(channel_id=channel_id, db=db):
-        raise HTTPException(status_code=404, detail="Channel not found")
-    return releases_service.create_release_type(name=name, platform_id=platform_id, channel_id=channel_id, db=db)
+						platform_id: int,
+						channel_id: int,
+						current_user: get_current_user,
+						db: db_session):
+	if current_user.role not in [RolesEnum.ADMIN, RolesEnum.RELEASE_MANAGER]:
+		raise HTTPException(status_code=403, detail="Not enough permissions")
+	if not get_platform(platform_id=platform_id, db=db):
+		raise HTTPException(status_code=404, detail="Platform not found")
+	if not get_channel(channel_id=channel_id, db=db):
+		raise HTTPException(status_code=404, detail="Channel not found")
+	return releases_service.create_release_type(name=name, platform_id=platform_id, channel_id=channel_id, db=db)
--- app/schemas.py
+++ app/schemas.py
@@ -8,164 +8,180 @@ from sql_app.models.releases import ReleaseStageEnum
 
 # Модели для передачи данных через API
 class ReleaseStageCreate(BaseModel):
-    name: str
-    description: str
-    start_date: str
-    end_date: str
-    status: str
-    platform_id: int
-    channel_id: int
-    release_type_id: int
-
-    class Config:
-        orm_mode = True
+	name: str
+	description: str
+	start_date: str
+	end_date: str
+	status: str
+	platform_id: int
+	channel_id: int
+	release_type_id: int
+	
+	class Config:
+		orm_mode = True
 
 
 class ReleaseStageOut(BaseModel):
-    id: int
-    name: str
-    description: str
-    start_date: datetime.datetime
-    end_date: datetime.datetime
-    status: ReleaseStageEnum
-    platform_id: int
-    channel_id: int
-    release_type_id: int
-
-    class Config:
-        orm_mode = True
+	id: int
+	name: str
+	description: str
+	start_date: datetime.datetime
+	end_date: datetime.datetime
+	status: ReleaseStageEnum
+	platform_id: int
+	channel_id: int
+	release_type_id: int
+	
+	class Config:
+		orm_mode = True
 
 
 class ReleaseTypeOut(BaseModel):
-    id: int
-    name: str
-    platform_id: int
-    channel_id: int
-
-    class Config:
-        orm_mode = True
+	id: int
+	name: str
+	platform_id: int
+	channel_id: int
+	
+	class Config:
+		orm_mode = True
 
 
 class UserCreate(BaseModel):
-    username: str
-    email: str
-    password: str
-    role: str
-
-    class Config:
-        orm_mode = True
+	username: str
+	email: str
+	password: str
+	role: str
+	
+	class Config:
+		orm_mode = True
 
 
 class UserUpdate(BaseModel):
-    username: str = None
-    email: str = None
-    password: str = None
-
-    class Config:
-        orm_mode = True
+	username: str = None
+	email: str = None
+	password: str = None
+	
+	class Config:
+		orm_mode = True
 
 
 class User(BaseModel):
-    id: int
-    username: str
-    password: str
-    role: str
-
-    class Config:
-        orm_mode = True
+	id: int
+	username: str
+	password: str
+	role: str
+	
+	class Config:
+		orm_mode = True
 
 
 class UserInDB(User):
-    hashed_password: str
+	hashed_password: str
 
 
 class PlatformCreate(BaseModel):
-    name: str
+	name: str
 
 
 class PlatformOut(BaseModel):
-    id: int
-    name: str
-
-    class Config:
-        orm_mode = True
+	id: int
+	name: str
+	
+	class Config:
+		orm_mode = True
 
 
 class ChannelCreate(BaseModel):
-    name: str
+	name: str
 
 
 class ChannelOut(BaseModel):
-    id: int
-    name: str
-
-    class Config:
-        orm_mode = True
+	id: int
+	name: str
+	
+	class Config:
+		orm_mode = True
 
 
 class ReleaseTypeCreate(BaseModel):
-    name: str
-    platform_id: int
-    channel_id: int
+	name: str
+	platform_id: int
+	channel_id: int
 
 
 class TaskTypeCreate(BaseModel):
-    key_name: str
-    name: str
-    description: str
-    is_required: bool | None
+	key_name: str
+	name: str
+	description: str
+	is_required: bool | None
 
 
 class TaskTypeOut(BaseModel):
-    id: int
-    key_name: str
-    name: str
-    description: str
-    is_required: bool
-
-    class Config:
-        orm_mode = True
+	id: int
+	key_name: str
+	name: str
+	description: str
+	is_required: bool
+	
+	class Config:
+		orm_mode = True
 
 
 class FeatureTypeCreate(BaseModel):
-    name: str
-    description: str
+	name: str
+	description: str
 
 
 class TaskOut(BaseModel):
-    id: int
-    feature_id: int
-    task_type_id: int
-    status: str
-
-    class Config:
-        orm_mode = True
+	id: int
+	feature_id: int
+	task_type_id: int
+	status: str
+	
+	class Config:
+		orm_mode = True
 
 
 class FeatureTypeOut(BaseModel):
-    id: int
-    name: str
-    description: str
-
-    class Config:
-        orm_mode = True
+	id: int
+	name: str
+	description: str
+	
+	class Config:
+		orm_mode = True
 
 
 class FeatureCreate(BaseModel):
-    name: str
-    status: str
-    feature_type_id: int
-    release_id: int
+	name: str
+	status: str
+	feature_type_id: int
+	release_id: int
 
 
 class FeatureOut(BaseModel):
-    id: int
-    name: str
-    status: str
-    created_at: datetime.datetime
-    feature_type_id: int
-    release_id: int
+	id: int
+	name: str
+	status: str
+	created_at: datetime.datetime
+	feature_type_id: int
+	release_id: int
+	
+	class Config:
+		orm_mode = True
 
-    class Config:
-        orm_mode = True
 
+class ReleaseFeature(BaseModel):
+	id: int
+	name: str
+	feature_type_id: int
+	status: str
+	
+	class Config:
+		orm_mode = True
+
+
+class ReleaseStageOutWithFeature(ReleaseStageOut):
+	features: list[ReleaseFeature] | None
+	
+	class Config:
+		orm_mode = True
