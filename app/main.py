d80b24a94b234f56743debeb1056ac2dbf53d3ec
dimanoga <dima.yanvarev@gmail.com>
2025-01-16 18:48:30 +0300

    delete middleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,4 @@
-import json
-from urllib.request import Request
-
 from fastapi import FastAPI
-from fastapi.exceptions import RequestValidationError
-from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.responses import JSONResponse
 
 from routers import admin_router, releases_router, auth_router, channels_router, platforms_router, tasks_router, \
     features_router
@@ -18,25 +12,3 @@ app.include_router(features_router.router)
 app.include_router(tasks_router.router)
 app.include_router(channels_router.router)
 app.include_router(platforms_router.router)
-
-
-@app.exception_handler(RequestValidationError)
-async def dispatch(request: Request, call_next):
-    try:
-        response = await call_next(request)
-        return response
-    except RequestValidationError as exc:
-        error_details = exc.errors()
-        readable_errors = [
-            {
-                "loc": error["loc"],
-                "msg": error["msg"],
-                "type": error["type"]
-            }
-            for error in error_details
-        ]
-        print("Validation Error:", json.dumps(readable_errors, indent=2, ensure_ascii=False))
-        return JSONResponse(
-            status_code=422,
-            content={"detail": readable_errors}
-        )
